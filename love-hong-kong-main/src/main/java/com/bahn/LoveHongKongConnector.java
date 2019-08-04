7eb97943b208069be1baed25670eb8f81c638d7f
mike6649 <michaeltang6649@gmail.com>
2019-08-04 18:24:24 +0800

    fix pom

--- love-hong-kong-main/src/main/java/com/bahn/LoveHongKongConnector.java
+++ love-hong-kong-main/src/main/java/com/bahn/LoveHongKongConnector.java
@@ -1,11 +1,10 @@
 package com.bahn;
 
 import java.io.File;
+import java.net.URL;
 import java.util.List;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.WebElement;
 
 import com.bahn.bean.PhotoBackup;
 import com.bahn.excel.RnDCSVReader;
@@ -17,8 +16,14 @@ public class LoveHongKongConnector {
 	public static void main(String[] args) {
 		try {
 			new TaskKiller ("chromedriver.exe");
-			System.setProperty("webdriver.chrome.driver",
-					LoveHongKongConnector.class.getResource("/chromedriver.exe").getFile());
+			
+			URL f = LoveHongKongConnector.class.getResource("chromedriver.exe");
+			if (f == null){
+				System.setProperty("webdriver.chrome.driver",
+						LoveHongKongConnector.class.getClassLoader().getResource("chromedriver.exe").getFile());
+			} else {
+				System.setProperty("webdriver.chrome.driver", f.getFile());
+			}
 
 			RnDChromeDriver driver = new RnDChromeDriver();
 			RnDCSVReader reader = new RnDCSVReader(new File("C:\\Users\\michael\\Downloads\\backup-pics.csv"));
